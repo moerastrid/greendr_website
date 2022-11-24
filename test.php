fdf3dc184ae8daceeda019b0c0cd56e3bec3948a
Astrid Gijsje Geels <92726944+moerastrid@users.noreply.github.com>
2022-11-24 21:17:09 +0100

    Update test.php

--- test.php
+++ test.php
@@ -48,6 +48,7 @@
    .side {
  	flex: 30%;
  	background-color: #f1f1f1;
+	color: SeaGreen
  	padding: 20px;
 	}
    .main {
@@ -81,20 +82,18 @@
    <div class="row">
     <div class="side">
 	<h2>About Greendr</h2>
-	<h5>Greendr is an online matchmaking platform where greenhouse growers can connect with local peers and initiatives to share information and solutions about common problems they have encountered like seasonal heating/energy demands. Special emphasis is placed on early adopters of sustainable solutions, who are encouraged to offer tried and tested advice to those who are still unsure, empowering growers to collaborate instead of tackling problems alone</h5>
+	<h5>Greendr is an online matchmaking platform where greenhouse growers can connect with local peers and initiatives to share information and solutions about common problems they have encountered like seasonal heating/energy demands.</h5>
   </div>
   <div class="main">
-    <h2>TITLE HEADING</h2>
-    <h5>Title description, Dec 7, 2017</h5>
-    <div class="fakeimg" style="height:200px;">Image</div>
-    <p>Some text..</p>
-    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <h2>emPower planters</h2>
+    <h5>Meet early adaptors of sustainable solutions</h5>
+    <p>Our "emPower planters" are happy to offer you tried and tested advice and can give you insight how adapting to new technologies worked out for them.</p>
+    <p>No need to do it all by yourself, collaborating will empower you to make the change to sustainable energy.</p>
     <br>
-    <h2>TITLE HEADING</h2>
-    <h5>Title description, Sep 2, 2017</h5>
-    <div class="fakeimg" style="height:200px;">Image</div>
-    <p>Some text..</p>
-    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <h2>Growing together</h2>
+    <h5>I water you, you water me, we grow together</h5>
+    <p>Because different growers use different resources to energize, heat and cool their greenhouses, the highs and lows in energy can differ throughout the year.</p>
+    <p>Greendr enables you to exchange your get insight in your own needs and in the needs of the peers around you. This enables you to contact them to establish a connection to benefit from each other.</p>
   </div>
 </div>
 
