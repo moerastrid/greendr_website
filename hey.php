3fce9876694ec6aa57da5d950ea769cf827dc70d
root <root@ip-172-31-19-238.eu-central-1.compute.internal>
2022-11-26 13:50:37 +0000

    test

--- hey.php
+++ hey.php
@@ -1,8 +1,24 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hey Daoyi!</p>'; ?> 
- </body>
-</html>
+<?php
+$name1 = "Daoyi";
+$name2 = "Astrid";
+
+function myTest() {
+	global $name1, $name2;
+	$name2 = $name1;
+}
+
+myTest();
+echo "Hey $name1 $name2 !";
+
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <html>
 <body>
-
-    <h2>\nTeam emPowerPlants.\n</h2>
     <?php
-        include("hey.php");
+       	include("form.php");
 	include("test.php");
     ?>
-    <h2>This is content after including hey.php content</h2>
 </body>
 </html>
