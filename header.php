c0d350f9bff7e1312274388f5f24c6fb5e24ce2a
Astrid Gijsje Geels <92726944+moerastrid@users.noreply.github.com>
2022-11-26 15:41:05 +0100

    Update and rename general.php to header.php

--- header.php
+++ header.php
@@ -11,40 +11,14 @@
    body {
 	font-family: Arial, Helvetica, sans-sarif;
 	}
-
    .header {
 	padding: 80px;
 	text-align: center;
 	background: SeaGreen;
 	color: LightYellow;
 	}
-
    .header h1 {
 	font-size: 40px;
-	}
-   .navbar {
-	overflow: hidden;
-	background-color: #333;
-	}
-   .navbar a {
-	float: left;
-	display: block;
-	color: white;
-	text-align: center;
-	padding: 14px 20px;
-	text-decoration: none;
-	}
-   .navbar a.right {
-	float: right;
-	}
-   .navbar a:hover {
-	background-color: LightYellow;
-	color: DarkGreen;
-	}
-  @media screen and (max-width: 400px) {
-   .navbar a {
-	float: none;
-        width:100%;
 	}
 }
 }
@@ -55,12 +29,6 @@
 	<h1>Greendr</h1>
 	<p>For Greenhouse Growers</p>
    </div>
-   <div class="navbar">
-	<a href="#">Link</a>
-	<a href="#">Link</a>
-	<a href="#" class="right">Link</a>
-   </div>
- </div>
  </body>
 </html>
 
